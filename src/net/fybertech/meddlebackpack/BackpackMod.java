ef1dbba34954b1d6948adfb1bfe68584cce7b555
FyberOptic <fyberoptic@gmail.com>
2015-08-19 02:19:06 -0400

    Added crafting recipe

--- src/net/fybertech/meddlebackpack/BackpackMod.java
+++ src/net/fybertech/meddlebackpack/BackpackMod.java
@@ -3,6 +3,10 @@ package net.fybertech.meddlebackpack;
 import net.fybertech.meddleapi.MeddleAPI;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
 
 
 public class BackpackMod
@@ -15,6 +19,8 @@ public class BackpackMod
 	public void init()
 	{		
 		MeddleAPI.registerItem(5000, "meddleBackpack", backpackItem);
+		
+		CraftingManager.getInstance().addRecipe(new ItemStack(backpackItem), "LLL", "LCL", "LLL", Character.valueOf('L'), Items.leather, Character.valueOf('C'), Blocks.chest);
 		
 		proxy = (CommonProxy)MeddleAPI.createProxyInstance("net.fybertech.meddlebackpack.CommonProxy", "net.fybertech.meddlebackpack.ClientProxy");
 		proxy.init();
